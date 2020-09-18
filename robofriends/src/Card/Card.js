fed0947d386c866b71cbb7a3efbf7bb6f9d6211c
Daniel <danisan9490@gmail.com>
2020-09-18 20:03:13 +0200

    #3 Create Robots Cards

--- robofriends/src/Card/Card.js
+++ robofriends/src/Card/Card.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './Card.css'
 
-function Card() {
+function Card({ id, name, email }) {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
     </div>
   )
 }
