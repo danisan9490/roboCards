74f9c25297e28e5364f58a55bbb7561d3c7e38d2
Daniel <danisan9490@gmail.com>
2020-09-19 11:24:09 +0200

    #8 Add Robots Scroll

--- robofriends/src/App/App.js
+++ robofriends/src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList/CardList';
 import SearchBox from './SearchBox/SearchBox';
 import './App.css'
+import Scroll from './Scroll/Scroll';
 class App extends Component {
   constructor() {
     super()
@@ -22,8 +23,8 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
     if (this.state.robots.length === 0) {
       return <h1 className="tc">Loading...</h1>
@@ -32,7 +33,9 @@ class App extends Component {
         <div className="tc" >
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
         </div>
       )
     }
