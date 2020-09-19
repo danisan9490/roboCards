89d35a5e5e611841f718803296936b3642c1bb0c
Daniel <danisan9490@gmail.com>
2020-09-19 11:01:32 +0200

    #7 Add Fetch API

--- robofriends/src/App/App.js
+++ robofriends/src/App/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import CardList from './CardList/CardList';
 import SearchBox from './SearchBox/SearchBox';
-import { robots } from '../robots';
 import './App.css'
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = (e) => {
@@ -20,14 +25,18 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading...</h1>
+    } else {
+      return (
+        <div className="tc" >
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      )
+    }
 
-    return (
-      <div className="tc" >
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    )
   }
 
 }
